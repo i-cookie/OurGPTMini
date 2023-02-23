063d0ee899c2085ae8ee1503f33b6feebc6aba1a
i-cookie <2284874018@qq.com>
2023-02-23 21:59:42 +0800

    elementary chatting functionality

---
+++ pages/LGUlife/index.vue
@@ -0,0 +1,37 @@
+<template>
+	<institutionSelector @changeInst="changeInst"/>
+</template>
+
+<script>
+	import institutionSelector from '../../components/LGUlife/institutionSelector.vue'
+	export default {
+		components: {
+			institutionSelector
+		},
+		data() {
+			return {
+				instId: 0,
+				deptId: 0,
+				depts: [
+					{
+						id: 0,
+						name: '全部'
+					},
+					{
+						id: 1,
+						name:'学术部'
+					}
+				]
+			}
+		},
+		methods: {
+			changeInst(e) {
+				this.instId = e;
+				console.log(this.instId);
+			}
+		},
+	}
+</script>
+
+<style>
+</style>
